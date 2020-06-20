476ffc2c0daa4343393cd271f52afd2f40ac008f
WM GI <boyofhonor01@gmail.com>
2020-06-21 00:29:14 +0100

    third

--- indexs.php
+++ indexs.php
@@ -180,71 +180,7 @@ session_start();
 
 
 
-        <!-- ======= Contact Section ======= -->
-        <section id="contact">
-            <div class="container wow fadeInUp">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3 class="section-title">Contact Us</h3>
-                        <div class="section-title-divider"></div>
-                        <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                            accusantium doloremque</p>
-                    </div>
-                </div>
 
-                <div class="row justify-content-center">
-                    <div class="col-lg-3 col-md-4">
-                        <div class="info">
-                            <div>
-                                <i class="fa fa-map-marker"></i>
-                                <p>A108 Adam Street<br>New York, NY 535022</p>
-                            </div>
-
-                            <div>
-                                <i class="fa fa-envelope"></i>
-                                <p>info@example.com</p>
-                            </div>
-
-                            <div>
-                                <i class="fa fa-phone"></i>
-                                <p>+1 5589 55488 55s</p>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div class="col-lg-5 col-md-8">
-                        <div class="form">
-                            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                                <div class="form-group">
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div class="validate"></div>
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                                    <div class="validate"></div>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                                    <div class="validate"></div>
-                                </div>
-                                <div class="form-group">
-                                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                                    <div class="validate"></div>
-                                </div>
-                                <div class="mb-3">
-                                    <div class="loading">Loading</div>
-                                    <div class="error-message"></div>
-                                    <div class="sent-message">Your message has been sent. Thank you!</div>
-                                </div>
-                                <div class="text-center"><button type="submit">Send Message</button></div>
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </section><!-- End Contact Section -->
 
         <script src="/vendor/jquery/jquery.min.js"></script>
         <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -265,8 +201,8 @@ session_start();
     <!--Modal: Login / Register Form /elegantModalForm-->
 
     <div class="text-center">
-        <a href="" class="btn btn-default btn-rounded my-3" data-toggle="modal" data-target="#modalLRForm">Launch
-            Modal LogIn/Register</a>
+        <a href="" class="btn btn-default btn-rounded my-3" data-toggle="modal" data-target="#modalLRForm">
+        </a>
     </div>
     <div class="modal fade" id="elegantModalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
